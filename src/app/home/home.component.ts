be74bcaacf37d3cfd03ee3bd91c62713f2f93c15
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2023-10-07 18:53:11 +0800

    新增 HTTP 通訊

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,22 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
       <!-- 迭代 housingLocationList 陣列中的每一項，可以取 let item of housingLocationList -->
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
-      >
-      </app-housing-location>
+      ></app-housing-location>
       <!-- [attribute] = "value"-->
       <!-- 將名為 "housingLocation" 的屬性值傳遞給名為 "app-housing-location" 的元件。  -->
       <!-- 左邊的 housingLocation 是 app-housing-location 元件的輸入屬性 -->
@@ -36,6 +41,25 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService); //透過 inject(HousingService) 方法手動注入了 HousingService 服務。
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    //頁面載入時，filteredLocationList應預設包含所有建築物位置值
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+
+  filteredLocationList: HousingLocation[] = []; //過濾結果屬性
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
