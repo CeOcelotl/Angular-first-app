1db437cab14179c35a6b3cc03dbd98737c1e7d61
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2023-10-07 13:12:34 +0800

    新增輸入引數

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -13,5 +14,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation; //輸入屬性housingLocation，其型別為HousingLocation。
 }
+//使用@Input裝飾器來聲明一個輸入屬性housingLocation，表示該屬性的值可以從外部傳入組件中。
+//型別註記"!"，因為這個輸入屬性期望有設定值。在這種情況下，沒有合適的預設值。 告訴TS 編譯器此屬性的值不能是null或undefined。
